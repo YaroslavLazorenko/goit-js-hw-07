b7cc6774487b6b5469583860a558a7b4bc259610
YaroslavLazorenko <lazorenko85@gmail.com>
2021-09-04 15:40:46 +0300

    Complete task 4

--- js/task-04.js
+++ js/task-04.js
@@ -1,3 +1,27 @@
-const value = document.getElementById('value');
+const counterSpan = document.getElementById('value');
 const decButton = document.querySelector('button[data-action="decrement"]');
 const incButton = document.querySelector('button[data-action="increment"]');
+
+let counterValue = Number(counterSpan.textContent); // Get initial value of counter from markup
+
+const decrement = () => {
+  counterValue -= 1;
+};
+
+const increment = () => {
+  counterValue += 1;
+};
+
+const updateCounterSpanValue = () => {
+  counterSpan.textContent = counterValue;
+};
+
+decButton.addEventListener('click', () => {
+  decrement();
+  updateCounterSpanValue();
+});
+
+incButton.addEventListener('click', () => {
+  increment();
+  updateCounterSpanValue();
+});
